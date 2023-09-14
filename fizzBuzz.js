a7db087382b2afb81f503fedd64bf880cad8800b
Spencer Cox <Spencercoxinbox@gmail.com>
2023-09-14 09:09:52 -0600

    initial commit

---
+++ fizzBuzz.js
@@ -0,0 +1,22 @@
+// Write your solution below this line:
+
+let num = 1;
+
+while (num < 51){
+
+    if (num % 3 === 0 && num % 5 === 0){
+        console.log("fizzbuzz");
+
+    } else if (num % 3 === 0){
+        console.log("fizz");
+
+    } else if (num % 5 === 0){
+        console.log("buzz");
+
+    } else {
+        console.log(num);
+    }
+
+   
+    num++;
+}
